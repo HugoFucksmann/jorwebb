65f3e70e5db69b7f289f406ac6d1e244be7c8a5c
colo <hugoffuksmann@gmail.com>
2021-07-16 01:47:48 -0300

    v1.9.9

--- models/prensa.js
+++ models/prensa.js
@@ -1,46 +1,45 @@
-const { Schema } = require("mongoose");
+const { Schema, model } = require('mongoose');
 
 const getFecha = () => {
-  return new Date();
+	return new Date();
 };
 
 const PrensaSchema = Schema(
-  {
-    title: {
-      type: String,
-      required: true,
-    },
-    subTitle: {
-      type: String,
-    },
-    text: {
-      type: String,
-      required: true,
-    },
-    img: {
-      type: String,
-      default: "",
-    },
-    proyectType: {
-      type: String,
-    },
-    date: {
-      type: Date,
-      default: getFecha(),
-      required: true,
-    },
-    usuarioCarga: {
-      type: String,
-    },
-  },
-  { collection: "prensa" }
+	{
+		title: {
+			type: String,
+			required: true,
+		},
+		subTitle: {
+			type: String,
+		},
+		text: {
+			type: String,
+			required: true,
+		},
+		img: {
+			type: String,
+			default: '',
+		},
+		proyectType: {
+			type: String,
+		},
+		date: {
+			type: Date,
+			default: getFecha(),
+			required: true,
+		},
+		usuarioCarga: {
+			type: String,
+		},
+	},
+	{ collection: 'prensa' }
 );
 PrensaSchema.ensureIndexes;
 
-PrensaSchema.method("toJSON", function () {
-  const { __v, expire, ...Object } = this.toObject();
+PrensaSchema.method('toJSON', function () {
+	const { __v, expire, ...Object } = this.toObject();
 
-  return Object;
+	return Object;
 });
-
-module.exports = PrensaSchema;
+module.exports = model('Prensa', PrensaSchema);
