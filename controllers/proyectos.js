f640a62f10958353a2a4b9419c6b790779cf8b11
hugofucksmann <hugoffuksmann@gmail.com>
2021-07-19 10:11:36 -0300

    fix

--- controllers/proyectos.js
+++ controllers/proyectos.js
@@ -1,102 +1,97 @@
-const Proyecto = require('../models/proyecto');
-const Usuario = require('../models/usuario');
+const Proyecto = require("../models/proyecto");
 
 const getProyectos = async (req, res) => {
-	const proyecto = await Proyecto.find();
-	res.json({
-		ok: true,
-		proyecto,
-	});
+  const proyecto = await Proyecto.find();
+  res.json({
+    ok: true,
+    proyecto,
+  });
 };
 
 const crearProyecto = async (req, res) => {
-	const proyecto = new Proyecto({
-		...req.body,
-	});
+  const proyecto = new Proyecto({
+    ...req.body,
+  });
 
-	try {
-		const proyectoDB = await proyecto.save();
+  try {
+    const proyectoDB = await proyecto.save();
 
-		res.json({
-			ok: true,
-			proyecto: proyectoDB,
-		});
-	} catch (error) {
-		console.log(error);
-		res.status(500).json({
-			ok: false,
-			msg: 'error inesperado, hable con el administrador',
-		});
-	}
+    res.json({
+      ok: true,
+      proyecto: proyectoDB,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      ok: false,
+      msg: "error inesperado, hable con el administrador",
+    });
+  }
 };
 
 const actualizarProyecto = async (req, res) => {
-	const proyectoId = req.params.id;
+  const proyectoId = req.params.id;
 
-	try {
-		const proyectoDB = await Proyecto.findById(proyectoId);
-		if (!proyectoDB) {
-			return res.status(404).json({
-				ok: false,
-				msg: 'Proyecyo no encontrada',
-			});
-		}
-		const cambiosPRoyecto = {
-			...req.body,
-		};
+  try {
+    const proyectoDB = await Proyecto.findById(proyectoId);
+    if (!proyectoDB) {
+      return res.status(404).json({
+        ok: false,
+        msg: "Proyecyo no encontrada",
+      });
+    }
+    const cambiosPRoyecto = {
+      ...req.body,
+    };
 
-		const proyectoActualizado = await Proyecto.findByIdAndUpdate(
-			proyectoId,
-			cambiosPRoyecto,
-			{
-				new: true,
-			}
-		);
+    const proyectoActualizado = await Proyecto.findByIdAndUpdate(proyectoId, cambiosPRoyecto, {
+      new: true,
+    });
 
-		res.json({
-			ok: true,
-			proyecto: proyectoActualizado,
-		});
-	} catch (error) {
-		console.log(error);
-		res.status(500).json({
-			ok: false,
-			msg: 'Error hable con el admin',
-		});
-	}
+    res.json({
+      ok: true,
+      proyecto: proyectoActualizado,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      ok: false,
+      msg: "Error hable con el admin",
+    });
+  }
 };
 
 const eliminarProyecto = async (req, res = response) => {
-	const noticiaId = req.params.id;
+  const noticiaId = req.params.id;
 
-	try {
-		const noticiasDB = await Proyecto.findById(noticiaId);
+  try {
+    const noticiasDB = await Proyecto.findById(noticiaId);
 
-		if (!noticiasDB) {
-			return res.status(404).json({
-				ok: false,
-				msg: 'noticia no encontrada',
-			});
-		}
+    if (!noticiasDB) {
+      return res.status(404).json({
+        ok: false,
+        msg: "noticia no encontrada",
+      });
+    }
 
-		await Proyecto.findByIdAndDelete(noticiaId);
+    await Proyecto.findByIdAndDelete(noticiaId);
 
-		res.json({
-			ok: true,
-			msg: 'noticia eliminada',
-		});
-	} catch (error) {
-		console.log(error);
-		res.status(500).json({
-			ok: false,
-			msg: 'Error hable con el admin',
-		});
-	}
+    res.json({
+      ok: true,
+      msg: "noticia eliminada",
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      ok: false,
+      msg: "Error hable con el admin",
+    });
+  }
 };
 
 module.exports = {
-	getProyectos,
-	crearProyecto,
-	actualizarProyecto,
-	eliminarProyecto,
+  getProyectos,
+  crearProyecto,
+  actualizarProyecto,
+  eliminarProyecto,
 };
