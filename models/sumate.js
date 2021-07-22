dc9cd87ac8ad507aa47223fb4c9f037dcb840d04
colo <hugoffuksmann@gmail.com>
2021-07-22 03:06:17 -0300

    fixed v2.3

--- models/sumate.js
+++ models/sumate.js
@@ -1,46 +1,46 @@
-const { Schema, model } = require("mongoose");
+const { Schema, model } = require('mongoose');
 
 const getFecha = () => {
-  return new Date();
+	return new Date();
 };
 
 const sumateSchema = Schema(
-  {
-    nombre: {
-      type: String,
-      require: true,
-    },
-    apellido: {
-      type: String,
-      required: true,
-    },
-    dni: {
-      type: String,
-    },
-    tel: {
-      type: String,
-      required: true,
-    },
-    email: {
-      type: String,
-      require: true,
-    },
-    ayuda: {
-      type: String,
-      require: true,
-    },
-    date: {
-      type: Date,
-      default: getFecha(),
-    },
-  },
-  { collection: "sumate" }
+	{
+		nombre: {
+			type: String,
+			require: true,
+		},
+		apellido: {
+			type: String,
+			required: true,
+		},
+		dni: {
+			type: String,
+		},
+		tel: {
+			type: String,
+			required: true,
+		},
+		email: {
+			type: String,
+			require: true,
+		},
+		ayuda: {
+			type: String,
+			require: true,
+		},
+		date: {
+			type: Date,
+			default: getFecha(),
+		},
+	},
+	{ collection: 'sumate' }
 );
 sumateSchema.ensureIndexes;
 
-sumateSchema.method("toJSON", function () {
-  const { __v, expire, ...Object } = this.toObject();
+sumateSchema.method('toJSON', function () {
+	const { __v, expire, ...Object } = this.toObject();
 
-  return Object;
+	return Object;
 });
-module.exports = model("Sumate", sumateSchema);
+module.exports = model('Sumate', sumateSchema);
--- routes/sumate.js
+++ routes/sumate.js
@@ -1,20 +1,22 @@
-const { Router } = require("express");
-const { check } = require("express-validator");
-const { validarCampos } = require("../middlewares/validar-campos");
-const { validarJWT } = require("../middlewares/validar-jwt");
+const { Router } = require('express');
+const { check } = require('express-validator');
+const { validarCampos } = require('../middlewares/validar-campos');
+const { validarJWT } = require('../middlewares/validar-jwt');
 
 const {
-  getSumate,
-  crearSumate,
-  eliminarSumate,
-  actualizarSumate,
-} = require("../controllers/prensa");
+	getSumate,
+	crearSumate,
+	eliminarSumate,
+	actualizarSumate,
+} = require('../controllers/prensa');
 
 const router = Router();
 
-router.get("/", validarJWT, getSumate);
-router.post("/", validarJWT, crearSumate);
-router.delete("/:id", validarJWT, eliminarSumate);
-router.patch("/:id", validarJWT, actualizarSumate);
+//router.get('/', validarJWT, getSumate);
+router.post('/', function (req, res) {
+	crearSumate;
+});
+//router.delete('/:id', validarJWT, eliminarSumate);
+//router.patch('/:id', validarJWT, actualizarSumate);
 
 module.exports = router;
