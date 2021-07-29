77ef22e2db03909acb1c016906ef9d8948b6eb39
colo <hugoffuksmann@gmail.com>
2021-07-29 02:42:01 -0300

    v2.6.6

--- controllers/sumate.js
+++ controllers/sumate.js
@@ -1,126 +1,134 @@
-const fetch = require("node-fetch");
-const Sumate = require("../models/sumate");
+const fetch = require('node-fetch');
+const Sumate = require('../models/sumate');
 
 const getSumate = async (req, res) => {
-  const sumate = await Sumate.find();
-  res.json({
-    ok: true,
-    sumate,
-  });
+	const sumate = await Sumate.find();
+	res.json({
+		ok: true,
+		sumate,
+	});
 };
 
 const crearSumate = async (req, res) => {
-  const token = req.body.token;
+	const token = req.body.token;
+	try {
+		const verify = await verifyRecaptcha(token);
 
-  try {
-    const verify = await verifyRecaptcha(token);
+		if (!verify)
+			return res.status(501).json({
+				ok: false,
+				msj: 'ocurrio un error al verificar token :(',
+			});
 
-    if (!verify)
-      return res.status(500).json({
-        ok: false,
-        msj: "ocurrio un error al verificar token :(",
-        verify: verify,
-      });
+		const sumate = new Sumate({
+			...req.body.sumate,
+		});
 
-    const sumate = new Sumate({
-      ...req.body.sumate,
-    });
+		const sumateDB = await sumate.save();
 
-    const sumateDB = await sumate.save();
-
-    res.json({
-      ok: true,
-      sumate: sumateDB,
-    });
-  } catch (error) {
-    console.log(error);
-    res.status(500).json({
-      ok: false,
-      msg: "error inesperado, hable con el administrador",
-    });
-  }
+		res.json({
+			ok: true,
+			sumate: sumateDB,
+		});
+	} catch (error) {
+		console.log(error);
+		res.status(500).json({
+			ok: false,
+			msg: 'error inesperado, hable con el administrador',
+		});
+	}
 };
 
 const actualizarSumate = async (req, res) => {
-  const sumateId = req.params.id;
+	const sumateId = req.params.id;
 
-  try {
-    const sumateDB = await Sumate.findById(sumateId);
-    if (!sumateDB) {
-      return res.status(404).json({
-        ok: false,
-        msg: "Proyecyo no encontrada",
-      });
-    }
-    const cambiosSumate = {
-      ...req.body,
-    };
+	try {
+		const sumateDB = await Sumate.findById(sumateId);
+		if (!sumateDB) {
+			return res.status(404).json({
+				ok: false,
+				msg: 'Proyecyo no encontrada',
+			});
+		}
+		const cambiosSumate = {
+			...req.body,
+		};
 
-    const sumateActualizado = await Sumate.findByIdAndUpdate(sumateId, cambiosSumate, {
-      new: true,
-    });
+		const sumateActualizado = await Sumate.findByIdAndUpdate(
+			sumateId,
+			cambiosSumate,
+			{
+				new: true,
+			}
+		);
 
-    res.json({
-      ok: true,
-      sumate: sumateActualizado,
-    });
-  } catch (error) {
-    console.log(error);
-    res.status(500).json({
-      ok: false,
-      msg: "Error hable con el admin",
-    });
-  }
+		res.json({
+			ok: true,
+			sumate: sumateActualizado,
+		});
+	} catch (error) {
+		console.log(error);
+		res.status(500).json({
+			ok: false,
+			msg: 'Error hable con el admin',
+		});
+	}
 };
 
 const eliminarSumate = async (req, res) => {
-  const sumateId = req.params.id;
+	const sumateId = req.params.id;
 
-  try {
-    const sumateDB = await Sumate.findById(sumateId);
+	try {
+		const sumateDB = await Sumate.findById(sumateId);
 
-    if (!sumateDB) {
-      return res.status(404).json({
-        ok: false,
-        msg: "persona no encontrada",
-      });
-    }
+		if (!sumateDB) {
+			return res.status(404).json({
+				ok: false,
+				msg: 'persona no encontrada',
+			});
+		}
 
-    await Sumate.findByIdAndDelete(sumateId);
+		await Sumate.findByIdAndDelete(sumateId);
 
-    res.json({
-      ok: true,
-      msg: "persona eliminada",
-    });
-  } catch (error) {
-    console.log(error);
-    res.status(500).json({
-      ok: false,
-      msg: "Error hable con el admin",
-    });
-  }
+		res.json({
+			ok: true,
+			msg: 'persona eliminada',
+		});
+	} catch (error) {
+		console.log(error);
+		res.status(500).json({
+			ok: false,
+			msg: 'Error hable con el admin',
+		});
+	}
 };
 
 const verifyRecaptcha = async (token) => {
-  // Validate Human
-  const isHuman = await fetch(`https://www.google.com/recaptcha/api/siteverify`, {
-    method: "post",
-    headers: {
-      Accept: "application/json",
-      "Content-Type": "application/x-www-form-urlencoded; charset=utf-8",
-    },
-    body: `secret=${RECAPTCHAKEY}&response=${token}`,
-  })
-    .then((res) => res.json())
-    .then((json) => json.success)
-    .catch((err) => {
-      throw new Error(`Error in Google Siteverify API. ${err.message}`);
-    });
+	// Validate Human
+	const isHuman = await fetch(
+		`https://www.google.com/recaptcha/api/siteverify`,
+		{
+			method: 'post',
+			headers: {
+				Accept: 'application/json',
+				'Content-Type':
+					'application/x-www-form-urlencoded; charset=utf-8',
+			},
+			body: `secret=${process.env.RECAPTCHAKEY}&response=${token}`,
+		}
+	)
+		.then((res) => res.json())
+		.then((json) => json.success)
+		.catch((err) => {
+			throw new Error(
+				`Error in Google Siteverify API. ${err.message}`
+			);
+		});
 
-  return isHuman;
+	return isHuman;
 };
 
 module.exports = {
-  crearSumate,
-  getSumate,
+	crearSumate,
+	getSumate,
 };
