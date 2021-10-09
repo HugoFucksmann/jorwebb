60946baaaacf04cc0e132e4083600266adadbbb2
Hugo Fucksmann <hugoffuksmann@gmail.com>
2021-10-09 12:43:21 -0300

    env en gitignore

--- database/config.js
+++ database/config.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const dbConnection = async () => {
   try {
-    await mongoose.connect(process.env.BD_CNN, {
+    await mongoose.connect(process.env.BD_CNN_JORAWEB, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
       // useCreateIndex: true,
