2735a8422b4dc5941196ee172c043469bee27613
colo <hugoffuksmann@gmail.com>
2021-11-15 11:45:37 -0300

    gg

--- BusCan/controllers/mascotas.js
+++ BusCan/controllers/mascotas.js
@@ -1,126 +1,135 @@
-const fetch = require("node-fetch");
-const { Mascota, Usuario } = require("../../database/busCanConnection");
-const AWS = require("aws-sdk");
-const { conectDrive, uploadFile, generatePublicUrl } = require("../../helpers/google-drive");
+const fetch = require('node-fetch');
+const {
+	Mascota,
+	Usuario,
+} = require('../../database/busCanConnection');
+const AWS = require('aws-sdk');
+const {
+	conectDrive,
+	uploadFile,
+	generatePublicUrl,
+	deleteFile,
+} = require('../../helpers/google-drive');
+const { comprimirImg } = require('../../helpers/compresorImg');
 
 function deg2rad(deg) {
-  return deg * (Math.PI / 180);
+	return deg * (Math.PI / 180);
 }
 
 function distKM(A, B) {
-  const R = 6371;
-  let aLat = parseFloat(A.location.latitude);
-  let aLon = parseFloat(A.location.longitude);
-  let bLat = parseFloat(B.location.latitude);
-  let bLon = parseFloat(B.location.longitude);
+	const R = 6371;
+	let aLat = parseFloat(A.location.latitude);
+	let aLon = parseFloat(A.location.longitude);
+	let bLat = parseFloat(B.location.latitude);
+	let bLon = parseFloat(B.location.longitude);
 
-  var dLat = 2 * R * Math.sin(deg2rad(aLat - bLat) / 2);
-  var dLon = 2 * R * Math.sin(deg2rad(aLon - bLon) / 2);
-  var dist = Math.sqrt(dLat ** 2 + dLon ** 2);
+	var dLat = 2 * R * Math.sin(deg2rad(aLat - bLat) / 2);
+	var dLon = 2 * R * Math.sin(deg2rad(aLon - bLon) / 2);
+	var dist = Math.sqrt(dLat ** 2 + dLon ** 2);
 
-  return dist;
+	return dist;
 }
 
 const getMascota2 = async (req, res) => {
-  let mascotas = await Mascota.find();
+	let mascotas = await Mascota.find();
 
-  try {
-    if (mascotas.length === 0) {
-      mascotas = false;
-      res.json({
-        ok: false,
-        mascotas,
-      });
-    } else {
-      res.json({
-        ok: true,
-        mascotas,
-      });
-    }
-  } catch (error) {
-    console.log(error);
-    res.status(400).json({
-      ok: false,
-      msg: "error hable con el admin",
-    });
-  }
+	try {
+		if (mascotas.length === 0) {
+			mascotas = false;
+			res.json({
+				ok: false,
+				mascotas,
+			});
+		} else {
+			res.json({
+				ok: true,
+				mascotas,
+			});
+		}
+	} catch (error) {
+		console.log(error);
+		res.status(400).json({
+			ok: false,
+			msg: 'error hable con el admin',
+		});
+	}
 };
 
 const getMascota = async (req, res) => {
-  const id = req.params.idUser;
+	const id = req.params.idUser;
 
-  let mascotas = await Mascota.find();
-  const usuario = await Usuario.findById(id);
+	let mascotas = await Mascota.find();
+	const usuario = await Usuario.findById(id);
 
-  try {
-    if (mascotas.length === 0) mascotas = false;
-    else
-      await mascotas.sort((a, b) => {
-        let dist2a = distKM(usuario, a);
-        let dist2b = distKM(usuario, b);
+	try {
+		if (mascotas.length === 0) mascotas = false;
+		else
+			await mascotas.sort((a, b) => {
+				let dist2a = distKM(usuario, a);
+				let dist2b = distKM(usuario, b);
 
-        return dist2a - dist2b;
-      });
+				return dist2a - dist2b;
+			});
 
-    res.json({
-      ok: true,
-      mascotas,
-    });
-  } catch (error) {
-    console.log(error);
-    res.status(400).json({
-      ok: false,
-      msg: "error hable con el admin",
-    });
-  }
+		res.json({
+			ok: true,
+			mascotas,
+		});
+	} catch (error) {
+		console.log(error);
+		res.status(400).json({
+			ok: false,
+			msg: 'error hable con el admin',
+		});
+	}
 };
 
-const getMiMascota = async (req, res = response) => {
-  const uid = req.params.id;
+const getMiMascota = async (req, res) => {
+	const uid = req.params.id;
 
-  const miMascota = await Mascota.findOne({ uid });
+	const miMascota = await Mascota.findOne({ uid });
 
-  try {
-    res.json({
-      ok: true,
-      miMascota,
-    });
-  } catch (error) {
-    console.log(error);
-    res.status(400).json({
-      ok: false,
-      msj: "no tienes mascota perdida",
-    });
-  }
+	try {
+		res.json({
+			ok: true,
+			miMascota,
+		});
+	} catch (error) {
+		console.log(error);
+		res.status(400).json({
+			ok: false,
+			msj: 'no tienes mascota perdida',
+		});
+	}
 };
 
-const crearMascota = async (req, res = response) => {
-  let messages = [];
-  const uid = req.body.uid; // se extrae del middleware validarToken
-  const mascota = new Mascota({
-    usuario: uid,
-    notification: req.body.notification,
-    ...req.body.perro,
-  });
+const crearMascota = async (req, res) => {
+	let messages = [];
+	const uid = req.body.uid; // se extrae del middleware validarToken
+	const mascota = new Mascota({
+		usuario: uid,
+		notification: req.body.notification,
+		...req.body.perro,
+	});
 
-  try {
-    const mascotaDB = await mascota.save();
-    const usuarios = await Usuario.find();
+	try {
+		const mascotaDB = await mascota.save();
+		const usuarios = await Usuario.find();
 
-    usuarios.map((usuario) => {
-      let distancia = distKM(usuario, mascotaDB);
+		usuarios.map((usuario) => {
+			let distancia = distKM(usuario, mascotaDB);
 
-      if (usuario._id !== uid && distancia < 6) {
-        messages.push({
-          to: usuario.notification,
-          sound: "default",
-          title: "se perdio una mascota !!",
-          body: "entra a ver para estar atento",
-        });
-      }
-    });
+			if (usuario._id !== uid && distancia < 6) {
+				messages.push({
+					to: usuario.notification,
+					sound: 'default',
+					title: 'se perdio una mascota !!',
+					body: 'entra a ver para estar atento',
+				});
+			}
+		});
 
-    /* 	await fetch('https://exp.host/--/api/v2/push/send', {
+		/* 	await fetch('https://exp.host/--/api/v2/push/send', {
 			method: 'POST',
 			headers: {
 				Accept: 'application/json',
@@ -130,81 +139,91 @@ const crearMascota = async (req, res = response) => {
 			body: JSON.stringify(messages),
 		}); */
 
-    res.json({
-      ok: true,
-      mascota: mascotaDB,
-    });
-  } catch (error) {
-    console.log(error);
-    res.status(500).json({
-      ok: false,
-      msg: "error inesperado, hable con el administrador",
-    });
-  }
+		res.json({
+			ok: true,
+			mascota: mascotaDB,
+		});
+	} catch (error) {
+		console.log(error);
+		res.status(500).json({
+			ok: false,
+			msg: 'error inesperado, hable con el administrador',
+		});
+	}
 };
 
-const crearMascotaf = async (req, res = response) => {
-  if (!req.files || Object.keys(req.files).length === 0) {
-    return res.status(400).json({
-      ok: false,
-      msj: "no hay ningun archivo subido",
-    });
-  }
+const crearMascotaf = async (req, res) => {
+	try {
+		if (!req.files || Object.keys(req.files).length === 0) {
+			return res.status(400).json({
+				ok: false,
+				msj: 'no hay ningun archivo subido',
+			});
+		}
 
-  const file = req.files.imgMascota;
-  const fileData = await comprimirImg(file.data);
+		const file = req.files.imgMascota;
+		const mascota = JSON.parse(req.body.mascota);
 
-  const nombreCortado = file.name.split(".");
-  const extensionArchivo = nombreCortado[nombreCortado.length - 1];
-  const extensionesValidas = ["png", "jpg", "jpeg"];
-  if (!extensionesValidas.includes(extensionArchivo)) {
-    return res.status(400).json({
-      ok: false,
-      msj: "No es una extension valida, usa .png .jpg o .jpeg",
-    });
-  }
-  const mid = req.body.perro.name;
-  const fileName = `${mid + "." + extensionArchivo}`;
-  const drive = conectDrive();
-  const result = await uploadFile(fileName, fileData, extensionArchivo);
+		const fileData = await comprimirImg(file.data);
 
-  if (!result) {
-    return res.status(400).json({
-      ok: false,
-      msj: "error al cargar imagen",
-    });
-  }
+		const nombreCortado = file.name.split('.');
+		const extensionArchivo = nombreCortado[nombreCortado.length - 1];
+		const extensionesValidas = ['png', 'jpg', 'jpeg'];
 
-  const url = await generatePublicUrl(drive, result.id);
+		if (!extensionesValidas.includes(extensionArchivo)) {
+			return res.status(400).json({
+				ok: false,
+				msj: 'No es una extension valida, usa .png .jpg o .jpeg',
+			});
+		}
 
-  let messages = [];
-  const uid = req.body.uid; // se extrae del middleware validarToken
+		const mName = JSON.stringify(mascota.date);
 
-  const mascota = new Mascota({
-    usuario: uid,
-    notification: req.body.notification,
-    petPicture: url.webContentLink,
-    ...req.body.perro,
-  });
+		const fileName = `${mName + '.' + extensionArchivo}`;
 
-  try {
-    const mascotaDB = await mascota.save();
-    const usuarios = await Usuario.find();
+		const drive = await conectDrive();
 
-    usuarios.map((usuario) => {
-      let distancia = distKM(usuario, mascotaDB);
+		const result = await uploadFile(
+			drive,
+			fileName,
+			fileData,
+			extensionArchivo
+		);
 
-      if (usuario._id !== uid && distancia < 3) {
-        messages.push({
-          to: usuario.notification,
-          sound: "default",
-          title: "se perdio una mascota !!",
-          body: "entra a ver para estar atento",
-        });
-      }
-    });
+		if (!result) {
+			return res.status(400).json({
+				ok: false,
+				msj: 'error al cargar imagen',
+			});
+		}
+		const url = await generatePublicUrl(drive, result.id);
+		const uid = mascota.usuario;
 
-    /* 	await fetch('https://exp.host/--/api/v2/push/send', {
+		const mascotaf = new Mascota({
+			...mascota,
+			petPicture: url.webContentLink,
+		});
+
+		const mascotaDB = await mascotaf.save();
+
+		let messages = [];
+
+		const usuarios = await Usuario.find();
+
+		usuarios.map((usuario) => {
+			let distancia = distKM(usuario, mascotaDB);
+
+			if (usuario._id !== uid && distancia < 2) {
+				messages.push({
+					to: usuario.notification,
+					sound: 'default',
+					title: 'se perdio una mascota !!',
+					body: 'entra a ver para estar atento',
+				});
+			}
+		});
+
+		await fetch('https://exp.host/--/api/v2/push/send', {
 			method: 'POST',
 			headers: {
 				Accept: 'application/json',
@@ -212,224 +231,263 @@ const crearMascotaf = async (req, res = response) => {
 				'Content-Type': 'application/json',
 			},
 			body: JSON.stringify(messages),
-		}); */
+		});
 
-    res.json({
-      ok: true,
-      mascota: mascotaDB,
-    });
-  } catch (error) {
-    console.log(error);
-    res.status(500).json({
-      ok: false,
-      msg: "error inesperado, hable con el administrador",
-    });
-  }
+		res.json({
+			ok: true,
+			mascota: mascotaDB,
+		});
+	} catch (error) {
+		console.log(error);
+		res.status(400).json({
+			ok: false,
+			msg: 'error al crear publicacion',
+		});
+	}
 };
 
 const actualizarMascota = async (req, res = response) => {
-  const mascotaId = req.params.id;
+	const mascotaId = req.params.id;
 
-  const { dist, ...newM } = req.body;
+	const { dist, ...newM } = req.body;
 
-  try {
-    const mascotaDB = await Mascota.findById(mascotaId);
+	try {
+		const mascotaDB = await Mascota.findById(mascotaId);
 
-    if (!mascotaDB) {
-      return res.status(404).json({
-        ok: false,
-        msg: "mascota no encontrada",
-      });
-    }
+		if (!mascotaDB) {
+			return res.status(404).json({
+				ok: false,
+				msg: 'mascota no encontrada',
+			});
+		}
 
-    const cambiosmascota = {
-      ...newM,
-    };
+		const cambiosmascota = {
+			...newM,
+		};
 
-    const mascotaActualizado = await Mascota.findByIdAndUpdate(mascotaId, cambiosmascota, {
-      new: true,
-    });
+		const mascotaActualizado = await Mascota.findByIdAndUpdate(
+			mascotaId,
+			cambiosmascota,
+			{
+				new: true,
+			}
+		);
 
-    res.json({
-      ok: true,
-      mascota: mascotaActualizado,
-    });
-  } catch (error) {
-    console.log(error);
-    res.status(500).json({
-      ok: false,
-      msg: "Error hsble con el admin",
-    });
-  }
+		res.json({
+			ok: true,
+			mascota: mascotaActualizado,
+		});
+	} catch (error) {
+		console.log(error);
+		res.status(500).json({
+			ok: false,
+			msg: 'Error hsble con el admin',
+		});
+	}
 };
 
 const recargarMascota = async (req, res = response) => {
-  const { mid } = req.params;
+	const { mid } = req.params;
 
-  try {
-    const mascotaDB = await Mascota.findById(mid);
+	try {
+		const mascotaDB = await Mascota.findById(mid);
 
-    if (!mascotaDB) {
-      return res.status(404).json({
-        ok: false,
-        msg: "mascota no encontrada",
-      });
-    }
+		if (!mascotaDB) {
+			return res.status(404).json({
+				ok: false,
+				msg: 'mascota no encontrada',
+			});
+		}
 
-    const reports = {
-      count: 0,
-      userId: [],
-    };
+		const reports = {
+			count: 0,
+			userId: [],
+		};
 
-    const mascotaActualizado = await Mascota.findByIdAndUpdate(
-      mid,
-      { report: reports },
-      { new: true }
-    );
+		const mascotaActualizado = await Mascota.findByIdAndUpdate(
+			mid,
+			{ report: reports },
+			{ new: true }
+		);
 
-    res.json({
-      ok: true,
-      mascota: mascotaActualizado,
-    });
-  } catch (error) {
-    console.log(error);
-    res.status(500).json({
-      ok: false,
-      msg: "Error hsble con el admin",
-    });
-  }
+		res.json({
+			ok: true,
+			mascota: mascotaActualizado,
+		});
+	} catch (error) {
+		console.log(error);
+		res.status(500).json({
+			ok: false,
+			msg: 'Error hsble con el admin',
+		});
+	}
 };
 
 const borrarMascota = async (req, res = response) => {
-  const mascotaId = req.params.id;
-  const s3 = new AWS.S3();
-  try {
-    const mascotaDB = await Mascota.findById(mascotaId);
-    if (!mascotaDB) {
-      return res.status(404).json({
-        ok: false,
-        msg: "mascota no encontrado",
-      });
-    }
-    const params = {
-      Bucket: "buscan",
-      Key: mascotaDB.petPicture.slice(42),
-    };
+	const mascotaId = req.params.id;
+	const s3 = new AWS.S3();
+	try {
+		const mascotaDB = await Mascota.findById(mascotaId);
+		if (!mascotaDB) {
+			return res.status(404).json({
+				ok: false,
+				msg: 'mascota no encontrado',
+			});
+		}
+		const params = {
+			Bucket: 'buscan',
+			Key: mascotaDB.petPicture.slice(42),
+		};
 
-    await s3.deleteObject(params, function (err, data) {
-      if (err) console.log(err, err.stack);
-      else console.log(data);
-    });
+		await s3.deleteObject(params, function (err, data) {
+			if (err) console.log(err, err.stack);
+			else console.log(data);
+		});
 
-    await Mascota.findByIdAndDelete(mascotaId);
+		await Mascota.findByIdAndDelete(mascotaId);
 
-    res.json({
-      ok: true,
-      msg: "mascota eliminado",
-    });
-  } catch (error) {
-    console.log(error);
-    res.status(500).json({
-      ok: false,
-      msg: "Error hsble con el admin",
-    });
-  }
+		res.json({
+			ok: true,
+			msg: 'mascota eliminado',
+		});
+	} catch (error) {
+		console.log(error);
+		res.status(500).json({
+			ok: false,
+			msg: 'Error hsble con el admin',
+		});
+	}
+};
+
+const borrarMascotaf = async (req, res = response) => {
+	const mascotaId = req.params.id;
+
+	try {
+		const mascotaDB = await Mascota.findById(mascotaId);
+
+		if (!mascotaDB) {
+			return res.status(404).json({
+				ok: false,
+				msg: 'animal no encontrado',
+			});
+		}
+
+		const [beg, end] = mascotaDB.petPicture.split('id=');
+		const [fileId, rest] = end.split('&');
+
+		const drive = await conectDrive();
+		await Mascota.findByIdAndDelete(mascotaId);
+		await deleteFile(drive, fileId);
+		res.json({
+			ok: true,
+			msg: 'animal eliminado',
+		});
+	} catch (error) {
+		console.log(error);
+		res.status(500).json({
+			ok: false,
+			msg: 'Error hsble con el admin',
+		});
+	}
 };
 
 const getUserMascota = async (req, res) => {
-  const userId = req.params.uidm;
-  try {
-    const usuario = await Usuario.findById(userId);
+	const userId = req.params.uidm;
+	try {
+		const usuario = await Usuario.findById(userId);
 
-    if (usuario) {
-      res.json({
-        ok: true,
-        userNoti: usuario.notification,
-      });
-    } else {
-      res.status(400).json({
-        ok: false,
-        msj: "no hay usuario",
-      });
-    }
-  } catch (error) {
-    console.log(error);
-    res.status(400).json({
-      ok: false,
-      msj: "error ups",
-    });
-  }
+		if (usuario) {
+			res.json({
+				ok: true,
+				userNoti: usuario.notification,
+			});
+		} else {
+			res.status(400).json({
+				ok: false,
+				msj: 'no hay usuario',
+			});
+		}
+	} catch (error) {
+		console.log(error);
+		res.status(400).json({
+			ok: false,
+			msj: 'error ups',
+		});
+	}
 };
 
 const addReport = async (req, res) => {
-  const { uid, mid } = req.body;
+	const { uid, mid } = req.body;
 
-  try {
-    const mascotaDB = await Mascota.findById(mid);
+	try {
+		const mascotaDB = await Mascota.findById(mid);
 
-    if (!mascotaDB) {
-      return res.status(404).json({
-        ok: false,
-        msg: "mascota no encontrado",
-      });
-    }
+		if (!mascotaDB) {
+			return res.status(404).json({
+				ok: false,
+				msg: 'animal no encontrado',
+			});
+		}
 
-    let count = mascotaDB.report.count;
-    let reporters = mascotaDB.report.userId;
-    let valid = true;
+		let count = mascotaDB.report.count;
+		let reporters = mascotaDB.report.userId;
+		let valid = true;
 
-    reporters.map((ids) => {
-      if (ids === uid) valid = false;
-    });
+		reporters.map((ids) => {
+			if (ids === uid) valid = false;
+		});
 
-    if (valid === false)
-      return res.json({
-        ok: false,
-        msg: "ya reportaste esta publicacion",
-      });
+		if (valid === false)
+			return res.json({
+				ok: false,
+				msg: 'ya reportaste esta publicacion',
+			});
 
-    reporters.push(uid);
-    count++;
+		reporters.push(uid);
+		count++;
 
-    let newM = {
-      report: {
-        count: count,
-        userId: reporters,
-      },
-    };
+		let newM = {
+			report: {
+				count: count,
+				userId: reporters,
+			},
+		};
 
-    await Mascota.findByIdAndUpdate(mid, newM, {
-      new: true,
-    });
+		await Mascota.findByIdAndUpdate(mid, newM, {
+			new: true,
+		});
 
-    res.json({
-      ok: true,
-      msg: "ya se notifico tu reporte, gracais por realizarlo !!",
-    });
-  } catch (error) {
-    console.log(error);
-    res.status(500).json({
-      ok: false,
-      msg: "Error hsble con el admin",
-    });
-  }
+		res.json({
+			ok: true,
+			msg: 'ya se notifico tu reporte, gracais por realizarlo !!',
+		});
+	} catch (error) {
+		console.log(error);
+		res.status(500).json({
+			ok: false,
+			msg: 'Error hsble con el admin',
+		});
+	}
 };
 
-const expireDelete = async (pets) => {
+/* const expireDelete = async (pets) => {
   const expire = pets.filter((pet) => pet.expire <= Date.now());
   if (expire.length > 0) expire.map((pet) => deleteExpirePet(pet));
 };
 
-const deleteExpirePet = (pet) => {};
+const deleteExpirePet = (pet) => {
+  return {}
+}; */
 
 module.exports = {
-  getMascota,
-  crearMascota,
-  crearMascotaf,
-  actualizarMascota,
-  borrarMascota,
-  getMiMascota,
-  getMascota2,
-  addReport,
-  recargarMascota,
+	getMascota,
+	crearMascota,
+	crearMascotaf,
+	actualizarMascota,
+	borrarMascota,
+	borrarMascotaf,
+	getMiMascota,
+	getMascota2,
+	addReport,
+	recargarMascota,
 };
--- helpers/google-drive.js
+++ helpers/google-drive.js
@@ -1,87 +1,85 @@
-const { google } = require("googleapis");
-const path = require("path");
-const fs = require("fs");
+const { google } = require('googleapis');
+const path = require('path');
+const fs = require('fs');
+const { Stream } = require('stream');
 
 async function conectDrive() {
-  const auth2Client = new google.auth.OAuth2(
-    process.env.DRIVE_ID_CLIENT,
-    process.env.DRIVE_SECRET_ID,
-    process.env.DRIVE_REDIRECT_URL
-  );
+	const auth2Client = new google.auth.OAuth2(
+		process.env.DRIVE_ID_CLIENT,
+		process.env.DRIVE_SECRET_ID,
+		process.env.DRIVE_REDIRECT_URL
+	);
 
-  auth2Client.setCredentials({
-    refresh_token: process.env.DRIVE_REFRESH_TOKEN,
-  });
+	auth2Client.setCredentials({
+		refresh_token: process.env.DRIVE_REFRESH_TOKEN,
+	});
 
-  const drive = google.drive({
-    version: "v3",
-    auth: auth2Client,
-  });
+	const drive = google.drive({
+		version: 'v3',
+		auth: auth2Client,
+	});
 
-  return drive;
+	return drive;
 }
 
-async function uploadFile(drive, name = "gg.png", file, extencion = "image/png") {
-  const filee = path.join(__dirname, "../files/images.png");
-  try {
-    const response = await drive.files.create({
-      requestBody: {
-        name: name, //This can be name of your choice
-        mimeType: extencion,
-        parents: ["1XhqRrFY4eHZ4uoZqKqooYsEBJX42D6iQ"],
-      },
-      media: {
-        mimeType: extencion,
-        body: fs.createReadStream(filee),
-      },
-    });
-    return response.data;
-  } catch (error) {
-    console.log(error.message);
-    return false;
-  }
+async function uploadFile(drive, name, file, extencion) {
+	let bufferStream = new Stream.PassThrough();
+	bufferStream.end(file);
+	try {
+		const response = await drive.files.create({
+			requestBody: {
+				name: name,
+				mimeType: `image/${extencion}`,
+				parents: ['1XhqRrFY4eHZ4uoZqKqooYsEBJX42D6iQ'],
+			},
+			media: {
+				mimeType: `image/${extencion}`,
+				body: bufferStream,
+			},
+		});
+		return response.data;
+	} catch (error) {
+		console.log(error.message);
+		return false;
+	}
 }
 
 async function deleteFile(drive, fileId) {
-  try {
-    const response = await drive.files.delete({
-      fileId: fileId,
-    });
-    console.log(response.data, response.status);
-  } catch (error) {
-    console.log(error.message);
-  }
+	try {
+		const response = await drive.files.delete({
+			fileId: fileId,
+		});
+		console.log(response.data, response.status);
+	} catch (error) {
+		console.log(error.message);
+	}
 }
 
 async function generatePublicUrl(drive, fileId) {
-  try {
-    await drive.permissions.create({
-      fileId: fileId,
-      requestBody: {
-        role: "reader",
-        type: "anyone",
-      },
-    });
+	try {
+		await drive.permissions.create({
+			fileId: fileId,
+			requestBody: {
+				role: 'reader',
+				type: 'anyone',
+			},
+		});
 
-    /* 
-    webViewLink: View the file in browser
-    webContentLink: Direct download link 
-    */
-    const result = await drive.files.get({
-      fileId: fileId,
-      fields: "webViewLink, webContentLink",
-    });
-    console.log(result.data);
-    return result.data;
-  } catch (error) {
-    console.log(error.message);
-    return false;
-  }
+		const result = await drive.files.get({
+			fileId: fileId,
+			fields: 'webViewLink, webContentLink',
+		});
+
+		return result.data;
+	} catch (error) {
+		console.log(error.message);
+		return false;
+	}
 }
 
 module.exports = {
-  conectDrive,
-  uploadFile,
-  generatePublicUrl,
-  deleteFile,
+	conectDrive,
+	uploadFile,
+	generatePublicUrl,
+	deleteFile,
 };
