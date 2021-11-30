61b9d9de3672ad3deebd9ba1886d113776b03511
HugoFucksmann <hugoffuksmann@gmail.com>
2021-11-30 18:59:56 -0300

    modif grande - trabajo - fixgubs

--- atrForm/routes/sheet.js
+++ atrForm/routes/sheet.js
@@ -2,7 +2,7 @@ const { Router } = require("express");
 const { getDatosForm } = require("../controllers/sheet");
 const router = Router();
 
-router.post("/getuser/:umail", getDatosForm);
-router.post("/saverow/:umail", getDatosForm);
+router.get("/getuser/:umail", getDatosForm);
+router.get("/saverow/:umail", getDatosForm);
 
 module.exports = router;
